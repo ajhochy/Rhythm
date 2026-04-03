485a029680e191e8c412599d0e2607b3b849d763
ajhochy <ajhochy@gmail.com>
2026-04-03 09:58:44 -0700

    chore: delete orphaned stub and placeholder files

--- apps/desktop_flutter/lib/features/rhythms/services/rhythms_service.dart
+++
@@ -1,3 +0,0 @@
-class RhythmsService {
-  // TODO: Domain logic for rhythm grouping and cadence.
-}
--- apps/desktop_flutter/lib/features/weekly_planner/services/weekly_planning_service.dart
+++
@@ -1,3 +0,0 @@
-class WeeklyPlanningService {
-  // TODO: Merge one-off, recurring, project-generated, and shadow events.
-}
